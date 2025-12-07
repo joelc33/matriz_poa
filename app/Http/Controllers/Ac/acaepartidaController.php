6f3da3f8eb37b7964bb622fde0e8a7df71e58899
root <joelc33@pdvsa.com>
2025-12-07 13:06:41 -0400

    partida

--- app/Http/Controllers/Ac/acaepartidaController.php
+++ app/Http/Controllers/Ac/acaepartidaController.php
@@ -180,9 +180,7 @@ class acaepartidaController extends Controller
                             ->where('nu_numero', '=', $contenido)
                             ->first();
 
-                            var_dump( $consulta_ae); echo $contenido; exit();
-
-
+                            
                             if($consulta_ae) {
                    
                    
